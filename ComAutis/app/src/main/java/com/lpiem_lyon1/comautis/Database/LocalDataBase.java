c968c3c07651b198768035d00a4bfed98dc205f7
Alexis Dumont <alexis.dumont.xii@gmail.com>
2016-01-07 15:06:08 +0100

    Begin implementation of LocalDataBase

--- ComAutis/app/src/main/java/com/lpiem_lyon1/comautis/Database/LocalDataBase.java
+++ ComAutis/app/src/main/java/com/lpiem_lyon1/comautis/Database/LocalDataBase.java
@@ -1,7 +1,15 @@
 package com.lpiem_lyon1.comautis.Database;
 
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.lpiem_lyon1.comautis.Database.Table.ChildTable;
+import com.lpiem_lyon1.comautis.Models.Child;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by alexislp on 07/01/16.
@@ -18,14 +26,45 @@ public class LocalDataBase implements ILocalDataBase {
         mPreferences = preferences;
     }
 
+    //region Request
     @Override
     public void requestChild(RequestCallback callback) {
-        
+        Cursor cursor = mSQLiteDatabase.query(ChildTable.TABLE_NAME, null, null, null, null, null, null, null);
+        List<Child> childList = new ArrayList<>();
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                Child child = new ChildTable().fromCursor(cursor);
+                childList.add(child);
+                cursor.close();
+                return;
+            }
+        }
+        cursor.close();
+        if (callback != null)
+            callback.onResult(childList);
+
     }
 
     @Override
     public void requestChildById(String id, RequestCallback callback) {
-
+        if(id != "" && id != null) {
+            Cursor cursor = mSQLiteDatabase.query(ChildTable.TABLE_NAME, null, ChildTable.KEY_ID + "=?", new String[]{id}, null, null, null, null);
+            List<Child> childList = new ArrayList<>();
+            if (cursor != null) {
+                if (cursor.moveToFirst()) {
+                    Child child = new ChildTable().fromCursor(cursor);
+                    childList.add(child);
+                    cursor.close();
+                    return;
+                }
+            }
+            cursor.close();
+            if (callback != null)
+                callback.onResult(childList);
+        }
+        if (callback != null) {
+            callback.onError(new IllegalArgumentException("id is null"));
+        }
     }
 
     @Override
@@ -92,4 +131,6 @@ public class LocalDataBase implements ILocalDataBase {
     public void requestPageByChild(String childId, RequestCallback callback) {
 
     }
+
+    //endregion
 }
