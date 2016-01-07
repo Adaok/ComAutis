a812f8edb3fe80e9851e7dc65f0c6f40da1245e9
Alexisdum <alexis.dumont.xii@gmail.com>
2016-01-07 10:38:09 +0100

    Update DrawerItem.java

--- ComAutis/app/src/main/java/com/lpiem_lyon1/comautis/DrawerItem.java
+++ ComAutis/app/src/main/java/com/lpiem_lyon1/comautis/DrawerItem.java
@@ -8,25 +8,24 @@ public class DrawerItem {
     private String mName;
     private int mIconId;
 
-    public DrawerItem(String mName, int mIconId) {
-        this.mIconId = mIconId;
-        this.mName = mName;
+    public DrawerItem(String name, int iconId) {
+        this.mIconId = iconId;
+        this.mName = name;
     }
 
-    public int getmIconId() {
+    public int getIconId() {
         return mIconId;
     }
 
-    public void setmIconId(int mIconId) {
-        this.mIconId = mIconId;
+    public void setIconId(int iconId) {
+        this.mIconId = iconId;
     }
 
-    public String getmName() {
+    public String getName() {
         return mName;
     }
 
-    public void setmName(String mName) {
-        this.mName = mName;
+    public void setName(String name) {
+        this.mName = name;
     }
-
 }
