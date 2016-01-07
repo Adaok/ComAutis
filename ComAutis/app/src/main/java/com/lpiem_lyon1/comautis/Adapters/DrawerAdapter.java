45fc1045e799e35ec13dae3db20c0edc009b5aaa
Marco Loiodice <mloiodice13@gmail.com>
2016-01-07 10:09:56 +0100

    Create folder for future adapters in the app and the adapter for DrawerNavigationMenu

---
+++ ComAutis/app/src/main/java/com/lpiem_lyon1/comautis/Adapters/DrawerAdapter.java
@@ -0,0 +1,70 @@
+package com.lpiem_lyon1.comautis.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.lpiem_lyon1.comautis.DrawerItem;
+import com.lpiem_lyon1.comautis.R;
+
+import java.util.List;
+
+/**
+ * Created by marcoloiodice on 06/01/2016.
+ */
+public class DrawerAdapter extends BaseAdapter {
+
+    List<DrawerItem> mDrawerItemList;
+    Context mContext;
+
+    public DrawerAdapter(Context mContext, List<DrawerItem> mDrawerItemList) {
+        this.mContext = mContext;
+        this.mDrawerItemList = mDrawerItemList;
+    }
+
+    @Override
+    public int getCount(){
+        return mDrawerItemList.size();
+    }
+
+    @Override
+    public Object getItem(int position){
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position){
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        LinearLayout layout;
+        if (convertView == null){
+            layout = (LinearLayout) LayoutInflater.from(mContext).inflate(R.layout.drawer_item_row, null);
+        }else {
+            layout = (LinearLayout)convertView;
+        }
+
+        TextView textViewItemName = (TextView) layout.findViewById(R.id.tv_drawer_item);
+        ImageView iconViewTtem = (ImageView) layout.findViewById(R.id.icon_drawer_item);
+
+        textViewItemName.setText(mDrawerItemList.get(position).getmName());
+        iconViewTtem.setImageResource(mDrawerItemList.get(position).getmIconId());
+
+        return layout;
+    }
+
+    public List<DrawerItem> getmDrawerItemList() {
+        return mDrawerItemList;
+    }
+
+    public void setmDrawerItemList(List<DrawerItem> mDrawerItemList) {
+        this.mDrawerItemList = mDrawerItemList;
+    }
+}
