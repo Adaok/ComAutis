ab9dc19502c2266a9422665a85283f6b1c509ff6
Alexis Dumont <alexis.dumont.xii@gmail.com>
2016-01-07 21:17:39 +0100

    Fix drawer and ToolBar

--- ComAutis/app/src/main/java/com/lpiem_lyon1/comautis/BaseActivity.java
+++ ComAutis/app/src/main/java/com/lpiem_lyon1/comautis/BaseActivity.java
@@ -1,38 +1,109 @@
 package com.lpiem_lyon1.comautis;
 
-import android.content.Context;
-import android.os.Bundle;
+import android.content.Intent;
+import android.support.annotation.LayoutRes;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 
 import com.lpiem_lyon1.comautis.Adapters.DrawerAdapter;
 import com.lpiem_lyon1.comautis.Models.DrawerItem;
+import com.lpiem_lyon1.comautis.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class BaseActivity extends AppCompatActivity {
 
-    private Context mContext;
-    private List<DrawerItem> mDrawerItemList;
-    private ListView mDrawerListView;
+    private ListView navigationView;
+    private DrawerLayout fullLayout;
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle drawerToggle;
+    private int selectedNavItemId;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base);
-        mContext = getBaseContext();
+    public void setContentView(@LayoutRes int layoutResID) {
+        /**
+         * This is going to be our actual root layout.
+         */
+        fullLayout = (DrawerLayout) getLayoutInflater().inflate(R.layout.activity_base, null);
+        /**
+         * {@link FrameLayout} to inflate the child's view. We could also use a {@link android.view.ViewStub}
+         */
+        FrameLayout activityContainer = (FrameLayout) fullLayout.findViewById(R.id.activity_content);
+        getLayoutInflater().inflate(layoutResID, activityContainer, true);
 
-        //Find items in layout
-        mDrawerListView = (ListView)findViewById(R.id.list_view_drawer);
+        /**
+         * Note that we don't pass the child's layoutId to the parent,
+         * instead we pass it our inflated layout.
+         */
+        super.setContentView(fullLayout);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        navigationView = (ListView) findViewById(R.id.navigationView);
 
         //init drawer menu item
-        mDrawerItemList = new ArrayList<>();
+        List<DrawerItem> mDrawerItemList = new ArrayList<>();
         mDrawerItemList.add(new DrawerItem(getString(R.string.child_list),R.drawable.ic_child_face));
         mDrawerItemList.add(new DrawerItem(getString(R.string.library),R.drawable.ic_library));
 
         //init list view with menu items
-        DrawerAdapter drawerAdapter = new DrawerAdapter(mContext , mDrawerItemList);
-        mDrawerListView.setAdapter(drawerAdapter);
+        DrawerAdapter drawerAdapter = new DrawerAdapter(getBaseContext() , mDrawerItemList);
+        navigationView.setAdapter(drawerAdapter);
+
+        if (useToolbar())
+        {
+            setSupportActionBar(toolbar);
+        }
+        else
+        {
+            toolbar.setVisibility(View.GONE);
+        }
+
+        setUpNavView();
+    }
+
+    /**
+     * Helper method that can be used by child classes to
+     * specify that they don't want a {@link Toolbar}
+     * @return true
+     */
+    protected boolean useToolbar()
+    {
+        return true;
+    }
+
+    protected void setUpNavView()
+    {
+        if( useDrawerToggle()) { // use the hamburger menu
+            drawerToggle = new ActionBarDrawerToggle(this, fullLayout, toolbar,
+                    R.string.nav_drawer_opened,
+                    R.string.nav_drawer_closed);
+
+            fullLayout.setDrawerListener(drawerToggle);
+            drawerToggle.syncState();
+        } else if(useToolbar() && getSupportActionBar() != null) {
+            // Use home/back button instead
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(getResources()
+                    .getDrawable(R.drawable.abc_ic_ab_back_mtrl_am_alpha));
+        }
+    }
+
+    /**
+     * Helper method to allow child classes to opt-out of having the
+     * hamburger menu.
+     * @return
+     */
+    protected boolean useDrawerToggle()
+    {
+        return true;
     }
 }
---
+++ ComAutis/app/src/main/java/com/lpiem_lyon1/comautis/ChooseChildActivity.java
@@ -0,0 +1,16 @@
+package com.lpiem_lyon1.comautis;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.lpiem_lyon1.comautis.BaseActivity;
+
+public class ChooseChildActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
