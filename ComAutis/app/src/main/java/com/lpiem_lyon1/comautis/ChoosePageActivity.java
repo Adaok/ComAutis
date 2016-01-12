0c2ea306a2a95ef40f3a09a232805236e3187ddd
Marco Loiodice <mloiodice13@gmail.com>
2016-01-12 23:15:37 +0100

    add function delete page in database

--- ComAutis/app/src/main/java/com/lpiem_lyon1/comautis/ChoosePageActivity.java
+++ ComAutis/app/src/main/java/com/lpiem_lyon1/comautis/ChoosePageActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -41,6 +42,48 @@ public class ChoosePageActivity extends BaseActivity {
         mPageListView = (ListView) findViewById(R.id.lv_pages);
 
         loadPages();
+
+        //long click for delete request
+        mPageListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+
+                //Get layout inflater for dialog for delete child
+                LayoutInflater inflaterDeletePage = getLayoutInflater();
+
+                //Associate view to the layout modal dialog
+                View dialogDeleteLayout = inflaterDeletePage.inflate(R.layout.dialog_delete_page, null);
+                //Create a builder for the dialog
+                AlertDialog.Builder mDeleteBuilder = new AlertDialog.Builder(ChoosePageActivity.this);
+                //Associate the view to the builder
+                mDeleteBuilder.setView(dialogDeleteLayout);
+
+                //Setting Buttons Yes or No
+                // Setting Positive "Yes" Button
+                mDeleteBuilder.setPositiveButton(R.string.btn_ad_positive, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User pressed YES button.
+                        String idPageTarget = mListPage.get(position).getId();
+                        mLocalDb.deletePageById(idPageTarget, null);
+                        Toast.makeText(getApplicationContext(), "Page deleted",
+                                Toast.LENGTH_SHORT).show();
+                        loadPages();
+                    }
+                });
+                // Setting Negative "NO" Button
+                mDeleteBuilder.setNegativeButton(R.string.btn_ad_negative, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User pressed No button.
+                        Toast.makeText(getApplicationContext(), "Cancelled", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                //Creation AlertDialog and associate to the builder showed.
+                AlertDialog alertDialogDeleteChild = mDeleteBuilder.show();
+
+                return false;
+            }
+        });
 
 
         FloatingActionButton fabAddPage = (FloatingActionButton) findViewById(R.id.fab_create_page);
